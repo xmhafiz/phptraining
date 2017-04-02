27c3c8152e4a3ce8bb86e439cf7af5c75c55e529
yohafiz <mohd.yohafiz@gmail.com>
2017-04-02 09:22:49 +0800

    update html to show php ouput

--- examples/mysql/mysql2.php
+++ examples/mysql/mysql2.php
@@ -23,18 +23,17 @@ $result = mysqli_query($connection, $query);
 echo 'total rows is: ' . mysqli_num_rows($result);
 echo '<br><br>';
 
-// loop within result
-// show in tables
-
-echo '<table border="1">';
-while ($row = mysqli_fetch_assoc($result)) {
-    echo '<tr>';
-    echo '<td>' . $row['name'] . '</td>';
-    echo '<td>' . $row['price'] . '</td>';
-    echo '</tr>';
-}
-echo '</table>';
+// end php
 ?>
+
+<table border="1">
+    <?php while ($row = mysqli_fetch_assoc($result)): ?>
+    <tr>
+        <td><?php echo $row['name'] ?></td>
+        <td><?php echo $row['price'] ?></td>
+    </tr>
+    <?php endwhile; ?>
+</table>
 
 <br><br>
 <a href="form.php">Add product</a>
--- examples/mysql_with_functions/mysql2.php
+++ examples/mysql_with_functions/mysql2.php
@@ -12,21 +12,20 @@ $result = mysqli_query($connection, $query);
 // get number of rows
 echo 'total rows is: ' . mysqli_num_rows($result);
 echo '<br><br>';
-
-// loop within result
-// show in tables
-
-echo '<table border="1">';
-while ($row = mysqli_fetch_assoc($result)) {
-    echo '<tr>';
-    echo '<td>' . $row['name'] . '</td>';
-    echo '<td>' . $row['price'] . '</td>';
-    echo '<td> <a href="edit_form.php?id=' . $row['id'] . '">Edit</a></td>';
-    echo '<td> <a href="delete_form.php?id=' . $row['id'] . '">Delete</a></td>';
-    echo '</tr>';
-}
-echo '</table>';
+// end php
 ?>
+
+
+<table border="1">
+    <?php while ($row = mysqli_fetch_assoc($result)): ?>
+    <tr>
+        <td><?php echo $row['name'] ?></td>
+        <td><?php echo $row['price'] ?></td>
+        <td><a href="edit_form.php?id=<?php echo $row['id'] ?>">Edit</a></td>
+        <td><a href="delete_form.php?id=<?php echo $row['id'] ?>">Delete</a></td>
+    </tr>
+    <?php endwhile; ?>
+</table>
 
 <br><br>
 <a href="form.php">Add product</a>
